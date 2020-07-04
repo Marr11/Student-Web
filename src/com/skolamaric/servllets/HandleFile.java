5cff1bd1d856c120ffcd092ee1e1beff33e173d2
Mar <marrewithscissors@gmail.com>
2020-07-04 09:58:45 +0200

    File handle, download.

--- src/com/skolamaric/servllets/HandleFile.java
+++ src/com/skolamaric/servllets/HandleFile.java
@@ -1,6 +1,7 @@
 package com.skolamaric.servllets;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,15 +19,16 @@ import javax.servlet.http.Part;
 /**
  * Servlet implementation class UploadFile
  */
-@WebServlet(value = "/UploadFile")
+@WebServlet(value = "/HandleFile")
 @MultipartConfig
-public class UploadFile extends HttpServlet {
+public class HandleFile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final int ARBITARY_SIZE = 1048;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UploadFile() {
+    public HandleFile() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,8 +37,23 @@ public class UploadFile extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+			
+		String fileName = request.getParameter("name");
+		
+		response.setContentType("image/jpeg");
+		//response.setHeader("Content-disposition", "attachment; filename="+ fileName);
+ 
+        try(InputStream in = new FileInputStream(new File("c:/tempv/" +fileName));
+          OutputStream out = response.getOutputStream()) {
+        
+            byte[] buffer = new byte[ARBITARY_SIZE];
+         
+            int numBytesRead;
+            while ((numBytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, numBytesRead);
+            }
+        }
+		
 	}
 
 	/**
