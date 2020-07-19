efe48d99d292b8e1cdef92108a1e85e5caa80ed4
Mar <marrewithscissors@gmail.com>
2020-07-19 10:41:49 +0200

    Zavrsena atentifikacija

--- src/com/skolamaric/servllets/LoginServlet.java
+++ src/com/skolamaric/servllets/LoginServlet.java
@@ -17,47 +17,64 @@ import com.skolamaric.servis.AdministracijaKorisnika;
 @WebServlet("/vezbaSecurity/loginServlet.html")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    AdministracijaKorisnika  administracijaKorisnika = new AdministracijaKorisnika();
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-	
+	AdministracijaKorisnika administracijaKorisnika = new AdministracijaKorisnika();
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		
+
+		handleIsExistingUser(response, username);
+	}
+
+	private void handleIsExistingUser(HttpServletResponse response, String username) throws IOException {
 		if (administracijaKorisnika.isRegistered(username)) {
-			if(administracijaKorisnika.isAuthenticated(username, password)) {
-				User user = administracijaKorisnika.getUser(username);
-				if(user!=null) {
-					request.getSession().setAttribute("user", user);
-				}
-				if(user.getRole().equals(Roles.ADMIN)) {
-					
-					response.sendRedirect("/StudentWeb/vezbaSecurity/adminHomeServlet.html");
-				}else {
-					
-					response.sendRedirect("/StudentWeb/vezbaSecurity/homePageServlet.html");
-				}
-				
-			}else {
-				response.sendRedirect("/StudentWeb/vezbaSecurity/pogresanPassword.html");
-				
-			}
-		}else {
+			 
+		} else {
 			response.sendRedirect("/StudentWeb/vezbaSecurity/notRegistered.html");
-			
+
 		}
 	}
-	private void handleIsRegistered() {
-		 
+
+	private void handleAuthentication(HttpServletRequest request, HttpServletResponse response, String username,
+			String password) throws IOException {
+		if (administracijaKorisnika.isAuthenticated(username, password)) {
+			handleUserRole(request, response, username);
+
+		} else {
+			response.sendRedirect("/StudentWeb/vezbaSecurity/pogresanPassword.html");
+
+		}
+	}
+
+	private void handleUserRole(HttpServletRequest request, HttpServletResponse response, String username)
+			throws IOException {
+		User user = administracijaKorisnika.getUser(username);
+		 if (user != null) {
+			request.getSession().setAttribute("user", user);
+		 }
+		
+		switch (user.getRole()) {
+		case ADMIN:	response.sendRedirect("/StudentWeb/vezbaSecurity/adminHomeServlet.html");
+			        break;			
+		case USER:	response.sendRedirect("/StudentWeb/vezbaSecurity/homePageServlet.html");
+		            break;
+		default:    response.sendRedirect("/StudentWeb/vezbaSecurity/notAuthorized.html");
+		            break;
+		}
+		
 	}
 
 }
